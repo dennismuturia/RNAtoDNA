da81dfb0e7d0ed23b1ae543c9ade443369f2e2a8
dennismuturia <dmuturia2922@gmail.com>
2017-08-23 11:15:36 +0300

    Inputs check
    Trying to check whether i could input something in my programme. Am inputting a string.

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 package com.company;
+import java.util.Scanner;//This package is where the user will be used to input
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello world");
+        //Welcome Statement
+        System.out.println("RNA to DNA");
+
+        //Initialize the scanner object. Scanner is used in the inputs
+        Scanner myScanner = new Scanner(System.in);
+
+        //Input something
+        String myName = myScanner.next();
+
+        System.out.println("Your name is" + myName);
     }
 }
